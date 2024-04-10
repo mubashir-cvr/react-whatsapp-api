5737ce2a100f50b6ba5340383494be11ad20e55c
Mubashir V <44843238+mubashir-cvr@users.noreply.github.com>
2024-04-11 00:26:15 +0530

    Update ListUser.jsx

--- src/pages/ListUser.jsx
+++ src/pages/ListUser.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom'
 function ListUser() {
   return (
     <>
-      <div>List User</div>
-      <Link to={"/users"}>List User</Link>
-      <Link to={"/adduser"}>Add User</Link>
+    <div className="flex flex-row w-full h-10 md:h-14 justify-between items-center">
+        <Link to={"/users"}><div className="flex">List User</div></Link>
+        <Link to={"/adduser"}><div className="flex">Add User</div></Link>
+    </div> 
+    <div>List Users</div>
     </>
   );
 }
