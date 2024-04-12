84f919eb8efeb4d50b7a17343ba59171d79c7840
mubashir-cvr <mubashircheruvayoor@gmail.com>
2024-04-12 12:24:39 +0530

    Changes

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 const itemClass =
-  "flex h-10 md:h-14 items-center w-1/3 justify-center hover:bg-gray-100 border-r-2";
+  "flex h-10 md:h-14 items-center w-1/2 justify-center hover:bg-gray-100 border-r-2";
 const itemActiveClass =
-  "flex h-10 md:h-14 items-center w-1/3 justify-center bg-gray-100  border-r-2";
+  "flex h-10 md:h-14 items-center w-1/2 justify-center bg-gray-100  border-r-2";
 function UserLayout({ pageName, contentComponent }) {
   return (
     <div className="flex flex-col w-full h-full">
+      <div className="flex flex-1 justify-center items-center">
+        {contentComponent}
+      </div>
       <div className="h-10 md:h-14 flex w-full flex-row items-center bg-white border-b-2">
         <NavLink
           to="/users"
           className={pageName === "listUser" ? itemActiveClass : itemClass}
         >
-          List User
+            Users
         </NavLink>
-        <NavLink
+        {/* <NavLink
           to="/adduser"
           className={pageName === "addUser" ? itemActiveClass : itemClass}
         >
           Add User
-        </NavLink>
+        </NavLink> */}
         <NavLink
           to="/roles"
           className={pageName === "roles" ? itemActiveClass : itemClass}
         >
-          Roles
+          User Roles
         </NavLink>
-      </div>
-      <div className="flex flex-1 justify-center items-center">
-        {contentComponent}
       </div>
     </div>
   );
