e1b5d022c33eff7e44c9c4e65f42afd3232783fa
mubashir-cvr <mubashircheruvayoor@gmail.com>
2024-04-12 16:15:10 +0530

    Layout change footer

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,18 +5,16 @@ import Header from "./Header";
 
 function RootLayout() {
   return (
-    <div className="flex flex-col h-screen w-screen border-2">
-      <div className="flex flex-1 flex-row h-full bg-white border-2">
-          <SideBar />
-        <div className="flex flex-1">
-          <div className="flex flex-col bg-blue-300 w-full h-full">
-            <div className="flex h-16 w-full bg-white"><Header/></div>
-            <div className="flex flex-1 justify-center items-center h-full w-full bg-gray-400">
-              <Outlet />
-            </div>
-            <div className="flex items-center bg-white justify-center h-14">
-              Footer
-            </div>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-1 bg-white">
+        <SideBar />
+        <div className="flex flex-col flex-1 bg-blue-300">
+          <Header />
+          <div className="flex flex-1 justify-center items-center bg-gray-400">
+            <Outlet />
+          </div>
+          <div className="flex items-center justify-center bg-white h-14">
+            Footer
           </div>
         </div>
       </div>
