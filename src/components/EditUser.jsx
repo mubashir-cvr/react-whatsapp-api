5e5d7d51a0c0645305a162641fcb415df36275ca
mubashir-cvr <mubashircheruvayoor@gmail.com>
2024-04-19 20:14:19 +0530

    Used edit

--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -1,28 +1,31 @@
 import React, { useState, useEffect, useRef } from "react";
-import { BiSave } from "react-icons/bi";
-import { MdCancel } from "react-icons/md";
 import ModalHead from "./ModalHead";
 import Select from "react-select";
 import { API_URL } from "../const/constants";
-function UserAdd({ users, handleModalClose, setShowCreateModal, setUsers }) {
-  const firstInputRef = useRef(null);
-  const lastInputRef = useRef(null);
-  const [roles, setRoles] = useState([]);
-  const [newUser, setNewUser] = useState({
+import { BiSave } from "react-icons/bi";
+import { MdCancel } from "react-icons/md";
+function EditUser({
+  selectedUser,
+  handleModalClose,
+  selectedUserId,
+  setShowEditModal,
+  setUsers,
+  users,
+}) {
+  const [editUser, setEditUser] = useState({
     email: "",
     password: "",
     name: "",
     department: "",
     address: "",
     phoneNumber: "",
-    profilePicture: null,
-    user_type: "User type",
+    status: "",
+    user_type: "",
     role: "",
   });
-  const user_types = [
-    { name: "STAFF", value: "STAFF" },
-    { name: "ADMIN", value: "ADMIN" },
-  ];
+  const [roles, setRoles] = useState([]);
+  const firstInputRef = useRef(null);
+  const lastInputRef = useRef(null);
   useEffect(() => {
     const fetchRoles = async () => {
       const token = localStorage.getItem("token");
@@ -36,31 +39,65 @@ function UserAdd({ users, handleModalClose, setShowCreateModal, setUsers }) {
       setRoles(jsonResponse.data);
     };
     firstInputRef.current.focus();
+    setEditUser(selectedUser);
     fetchRoles();
   }, []);
 
-  const handleInputChange = (event) => {
+  const handleUserTypeChange = (selectedOption) => {
+    setEditUser((prevUser) => ({
+      ...prevUser,
+      user_type: selectedOption.value,
+    }));
+  };
+  const handleEditInputChange = (event) => {
     const { name, value, files } = event.target;
     const updatedValue = name === "profilePicture" ? files[0] : value;
-    setNewUser({ ...newUser, [name]: updatedValue });
+    setEditUser({ ...editUser, [name]: updatedValue });
   };
   const handleChange = (selectedOption) => {
-    setNewUser((prevUser) => ({
+    setEditUser((prevUser) => ({
       ...prevUser,
       role: selectedOption,
     }));
   };
 
-  const handleUserTypeChange = (selectedOption) => {
-    setNewUser((prevUser) => ({
-      ...prevUser,
-      user_type: selectedOption.value,
-    }));
+  const handleEditSubmit = async () => {
+    const token = localStorage.getItem("token");
+    const formData = new FormData();
+    Object.keys(editUser).forEach((key) => {
+      if (key === "role") {
+        formData.append(key, editUser[key]._id);
+      } else {
+        formData.append(key, editUser[key]);
+      }
+    });
+    const response = await fetch(API_URL + `auth/users/${selectedUserId}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+    if (response.ok) {
+      const updatedUser = await response.json();
+      const updatedUsers = users.map((user) =>
+        user._id === selectedUserId ? updatedUser.data : user
+      );
+      setUsers(updatedUsers);
+      setShowEditModal(false);
+    } else {
+      console.error("Failed to update user");
+    }
   };
+  const user_types = [
+    { name: "STAFF", value: "STAFF" },
+    { name: "ADMIN", value: "ADMIN" },
+  ];
+
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       if (event.target === lastInputRef.current) {
-        handleSubmit();
+        handleEditSubmit();
       } else {
         const form = event.target.form;
         const index = Array.prototype.indexOf.call(form, event.target);
@@ -70,59 +107,29 @@ function UserAdd({ users, handleModalClose, setShowCreateModal, setUsers }) {
       }
     }
   };
-  const handleSubmit = async () => {
-    const token = localStorage.getItem("token");
-    const formData = new FormData();
-    Object.keys(newUser).forEach((key) => {
-      if (key === "role") {
-        formData.append(key, newUser[key]._id);
-      } else {
-        formData.append(key, newUser[key]);
-      }
-    });
-
-    const response = await fetch(API_URL + "auth/users", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: formData,
-    });
-    if (response.ok) {
-      const updatedUser = await response.json();
-      console.log(updatedUser.data);
-      setUsers([...users, updatedUser.data]);
-      setShowCreateModal(false);
-      setNewUser({
-        email: "",
-        password: "",
-        name: "",
-        department: "",
-        address: "",
-        phoneNumber: "",
-        status: "",
-        profilePicture: null,
-        user_type: "",
-        role: "",
-      });
-    } else {
-      console.error("Failed to add user");
-    }
-  };
-
   return (
-    <div className="fixed inset-0 p-3 bg-slate-50 bg-opacity-50 flex items-center justify-center">
-      <div className="flex flex-col w-full md:w-96 bg-white p-4 shadow-md border-2">
-        <ModalHead heading={"Add User"} />
+    <div className="fixed p-3 inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white w-full md:w-96 p-4 rounded-lg">
+        <ModalHead heading={"Edit User"} />
         <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            disabled
+            type="email"
+            placeholder="Email"
+            className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+            name="email"
+            value={editUser.email}
+            onChange={handleEditInputChange}
+            onKeyDown={handleKeyDown}
+          />
           <input
             type="text"
             placeholder="Name"
             className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
             name="name"
-            value={newUser.name}
-            onChange={handleInputChange}
+            value={editUser.name}
             onKeyDown={handleKeyDown}
+            onChange={handleEditInputChange}
             ref={firstInputRef}
           />
           <input
@@ -130,26 +137,34 @@ function UserAdd({ users, handleModalClose, setShowCreateModal, setUsers }) {
             placeholder="Department"
             className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
             name="department"
-            value={newUser.department}
-            onChange={handleInputChange}
+            value={editUser.department}
             onKeyDown={handleKeyDown}
+            onChange={handleEditInputChange}
           />
-
+          <input
+            type="text"
+            placeholder="Address"
+            className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+            name="address"
+            value={editUser.address}
+            onKeyDown={handleKeyDown}
+            onChange={handleEditInputChange}
+          />
           <input
             type="text"
             placeholder="Phone Number"
             className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
             name="phoneNumber"
-            value={newUser.phoneNumber}
-            onChange={handleInputChange}
+            value={editUser.phoneNumber}
             onKeyDown={handleKeyDown}
+            onChange={handleEditInputChange}
           />
           <Select
             options={user_types}
             getOptionLabel={(option) => option.name}
             getOptionValue={(option) => option.value}
             placeholder="Select User type"
-            value={{"name":newUser.user_type,value:newUser.user_type}}
+            value={{ name: editUser.user_type, value: editUser.user_type }}
             onChange={handleUserTypeChange}
             onKeyDown={handleKeyDown}
           />
@@ -159,35 +174,25 @@ function UserAdd({ users, handleModalClose, setShowCreateModal, setUsers }) {
             getOptionLabel={(option) => option.name}
             getOptionValue={(option) => option._id}
             placeholder="Select Role"
-            value={newUser.role}
+            value={editUser.role}
             onChange={handleChange}
             onKeyDown={handleKeyDown}
           />
-         
+
           <input
-            type="email"
-            placeholder="Email"
-            className="border border-gray-300 rounded-md px-3 py-2 mt-2 w-full"
-            name="email"
-            value={newUser.email}
-            onChange={handleInputChange}
+            type="text"
+            placeholder="Status"
+            className="border mt-2 border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
+            name="status"
+            value={editUser.status}
             onKeyDown={handleKeyDown}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="border border-gray-300 rounded-md px-3 py-2 mt-2 w-full"
-            name="password"
-            value={newUser.password}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
+            onChange={handleEditInputChange}
             ref={lastInputRef}
           />
-
           <div className="flex  mt-4 justify-center">
             <div
               className="px-4 py-2 text-pink-900 border-2 rounded-md mr-2"
-              onClick={handleSubmit}
+              onClick={handleEditSubmit}
             >
               <BiSave className="text-lg" />
             </div>
@@ -204,4 +209,4 @@ function UserAdd({ users, handleModalClose, setShowCreateModal, setUsers }) {
   );
 }
 
-export default UserAdd;
+export default EditUser;
--- src/pages/ListUser.jsx
+++ src/pages/ListUser.jsx
@@ -4,6 +4,7 @@ import { SiPrintables } from "react-icons/si";
 import { API_URL } from "../const/constants";
 import LoadingUserCard from "../components/LoadingUserCard";
 import UserAdd from "../components/UserAdd";
+import EditUser from "../components/EditUser";
 function ListUser() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -64,14 +65,6 @@ function ListUser() {
     setSelectedUserId(null);
   };
 
-
-
-  const handleEditInputChange = (event) => {
-    const { name, value, files } = event.target;
-    const updatedValue = name === "profilePicture" ? files[0] : value;
-    setEditUser({ ...editUser, [name]: updatedValue });
-  };
-
   const deleteUser = async (userId) => {
     const token = localStorage.getItem("token");
     const response = await fetch(API_URL + `/auth/users/${userId}`, {
@@ -102,32 +95,6 @@ function ListUser() {
     }
   };
 
-  const handleEditSubmit = async () => {
-    const token = localStorage.getItem("token");
-    const formData = new FormData();
-    Object.keys(editUser).forEach((key) => {
-      formData.append(key, editUser[key]);
-    });
-
-    const response = await fetch(API_URL + `/auth/users/${selectedUserId}`, {
-      method: "PUT",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: formData,
-    });
-    if (response.ok) {
-      const updatedUser = await response.json();
-      const updatedUsers = users.map((user) =>
-        user._id === selectedUserId ? updatedUser : user
-      );
-      setUsers(updatedUsers);
-      setShowEditModal(false);
-    } else {
-      console.error("Failed to update user");
-    }
-  };
-
   return (
     <div className="flex h-full md:p-4 w-full md:w-5/12 overflow-scroll no-scrollbar flex-col items-center">
       <div className="flex flex-row gap-2 items-center justify-center text-pink-900 w-full h-12 border-2 mb-2">
@@ -146,7 +113,6 @@ function ListUser() {
             <LoadingUserCard />
             <LoadingUserCard />
             <LoadingUserCard />
-          
           </div>
         ) : (
           users.map((user, index) => (
@@ -170,103 +136,23 @@ function ListUser() {
       </div>
 
       {showCreateModal && (
-        <UserAdd users={users} handleModalClose={handleModalClose} setShowCreateModal={setShowCreateModal} setUsers={setUsers}/>
+        <UserAdd
+          users={users}
+          handleModalClose={handleModalClose}
+          setShowCreateModal={setShowCreateModal}
+          setUsers={setUsers}
+        />
       )}
 
       {showEditModal && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-          <div className="bg-white p-4 rounded-lg">
-            <h2 className="text-xl font-bold mb-4">Edit User</h2>
-            <input
-              type="email"
-              placeholder="Email"
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="email"
-              value={editUser.email}
-              onChange={handleEditInputChange}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="password"
-              value={editUser.password}
-              onChange={handleEditInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Name"
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="name"
-              value={editUser.name}
-              onChange={handleEditInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Department"
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="department"
-              value={editUser.department}
-              onChange={handleEditInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Address"
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="address"
-              value={editUser.address}
-              onChange={handleEditInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Phone Number"
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="phoneNumber"
-              value={editUser.phoneNumber}
-              onChange={handleEditInputChange}
-            />
-            <input
-              type="text"
-              placeholder="Status"
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="status"
-              value={editUser.status}
-              onChange={handleEditInputChange}
-            />
-            <input
-              type="text"
-              placeholder="User Type"
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="user_type"
-              value={editUser.user_type}
-              onChange={handleEditInputChange}
-            />
-            {/* Assuming role is selected from a dropdown */}
-            <select
-              className="border border-gray-300 rounded-md px-3 py-2 mb-2 w-full"
-              name="role"
-              value={editUser.role}
-              onChange={handleEditInputChange}
-            >
-              <option value="">Select Role</option>
-              {/* Map over roles if available */}
-            </select>
-            <div className="flex justify-end">
-              <button
-                className="px-4 py-2 bg-blue-500 text-white rounded-md mr-2"
-                onClick={handleEditSubmit}
-              >
-                Save
-              </button>
-              <button
-                className="px-4 py-2 bg-gray-300 rounded-md"
-                onClick={handleModalClose}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
+        <EditUser
+          selectedUser={editUser}
+          users={users}
+          handleModalClose={handleModalClose}
+          selectedUserId={selectedUserId}
+          setShowEditModal={setShowEditModal}
+          setUsers={setUsers}
+        />
       )}
     </div>
   );
