ec445d8fc465143277e895dc7c9cd80b6b5f9280
mubashir-cvr <mubashircheruvayoor@gmail.com>
2024-04-25 23:56:11 +0530

    Chnages

--- src/const/env_constant.jsx
+++ src/const/env_constant.jsx
@@ -1,1 +1,2 @@
-export const API_URL = 'https://api.marjintech.online/api/'
+const { BASE_URL, MODE, DEV, PROD, SSR } =import.meta.env
+export const API_URL = DEV ? 'http://localhost:3000/api/' : 'https://api.marjintech.online/api/'
