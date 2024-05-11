6a62f5e539f7318a570cb91e33a2d358f5a18c7c
mubashir-cvr <mubashircheruvayoor@gmail.com>
2024-05-11 16:08:54 +0530

    Changes

--- src/components/role/LoadingRoleCard.jsx
+++ src/components/role/LoadingRoleCard.jsx
@@ -3,25 +3,14 @@ import React from "react";
 function RoleCard() {
   return (
     <div
-      className="flex w-full min-h-16  animate-pulse  shadow-md max-h-32 flex-row text-xs justify-between text-pink-900 px-2 gap-4 items-center border-2 bg-white "
+      className="flex w-full opacity-25 animate-pulse  shadow-md max-h-32 flex-row text-xs justify-between text-black-900  items-center border bg-white "
     >
-      <p className="flex w-1/3 border-2 h-2 rounded-lg items-center px-4 justify-center"></p>
-      <div className="flex w-1/3 px-4 border-2 h-2 rounded-lg  overflow-scroll items-center no-scrollbar justify-center">
-      
+     <p className="flex w-1/3 border-r-2 h-full items-center justify-center">  .......</p>
+      <div className="flex w-1/3  border-r-2  h-full overflow-scroll items-center no-scrollbar justify-center">
+     .......
       </div>
-      <div className="flex gap-3 justify-center">
-      <button
-          
-          className=" text-pink-900 border-2 p-2 text-xs rounded-lg"
-        >
-       
-        </button>
-        <button
-         
-          className="text-pink-900 border-2 p-2 text-xs rounded-lg"
-        >
-          
-        </button>
+      <div className="flex py-2 w-1/3 gap-2 justify-center">
+      .......
       </div>
     </div>
   );
--- src/components/stock/LoadingStockCard.jsx
+++ src/components/stock/LoadingStockCard.jsx
@@ -2,14 +2,25 @@ import React from "react";
 
 function LoadingStockCard() {
   return (
-    <div className="flex w-full min-h-16  animate-pulse  shadow-md max-h-32 flex-row text-xs justify-between text-pink-900 px-2 gap-4 items-center border-2 bg-white ">
-      <div className="flex w-1/4 border-2 h-2 rounded-lg items-center px-4 justify-center"></div>
-      <div className="flex w-1/4 border-2 h-2 rounded-lg items-center px-4 justify-center"></div>
-      <div className="flex w-1/4 border-2 h-2 rounded-lg items-center px-4 justify-center"></div>
-      <div className="flex w-1/4 px-4 border-2 h-2 rounded-lg items-center justify-center"></div>
-      <div className="flex gap-3 justify-center">
-        <button className="text-pink-900 border-2 p-2 text-xs rounded-lg"></button>
-        <button className="text-pink-900 border-2 p-2 text-xs rounded-lg"></button>
+    <div className="flex w-full min-h-12 opacity-20 animate-pulse  shadow-md max-h-32 flex-row text-xs justify-between text-black-900 items-center border-2 bg-white ">
+        <p className="flex w-2/4 md:w-1/6 border-r-2 h-full items-center justify-center">
+        .....
+      </p>
+      <p className="flex  w-1/4 md:w-1/6 border-r-2 h-full items-center justify-center">
+      .....
+      </p>
+      <p className="flex  w-1/4 md:w-1/6 border-r-2 h-full items-center justify-center">
+      .....
+      </p>
+      <p className="hidden md:flex  md:w-1/6 border-r-2 h-full items-center justify-center">
+      .....
+      </p>
+      <p className="hidden md:flex  md:w-1/6 border-r-2 h-full items-center justify-center">
+      .....
+      </p>
+      {/* <p className="flex  md:w-1/6 justify-center">{stockItem.as_on_date}</p> */}
+      <div className="w-1/4 flex-row flex md:w-1/6 items-center py-1 gap-1  md:gap-3 justify-center">
+      .....
       </div>
     </div>
   );
